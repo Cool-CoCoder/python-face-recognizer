35949f253a1ec033edc23af8443bd51353f9a378
CV-Student <63215608+Cool-CoCoder@users.noreply.github.com>
2022-03-12 21:49:33 +0800

    Update opertator_1.py

--- image_operate/opertator_1.py
+++ image_operate/opertator_1.py
@@ -1,9 +1,16 @@
-
-# 截取原始图像感兴趣区域并转为灰度
-def Ori2StandardGray(path):
-    mat = cv.cvtColor(cv.imread(path),cv.COLOR_BGR2GRAY)
+# 截取原始图像感兴趣区域并覆盖源文件
+def Ori2Standard(path):
+    mat = cv.imread(path)
+    gray_mat = cv.cvtColor(mat,cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(mat,1.1,5,cv.CASCADE_SCALE_IMAGE,(400,400))
     for (x,y,w,h) in faces:
         mat = mat[y:y+h,x:x+w] # 这和画矩形不同，注意长和宽的顺序
         s_mat = cv.resize(mat,(250,250))
-        cv.imwrite("test.jpg",s_mat)
+        cv.imwrite(path,s_mat)
+
+
+# 清洗文件夹的图片，清洗整个文件夹图片
+def File2Standard(path):
+    f_list = os.listdir(path)
+    for f in f_list:
+        Ori2Standard(path+'/'+f)
